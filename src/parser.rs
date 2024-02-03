8b844b3a0f458f49ac8cc634cfd92b5ff642eb93
lynnlei <brooke.leichtle@gmail.com>
2024-02-03 22:29:24 +0100

    first steps to parsing attributes for tags

--- src/parser.rs
+++ src/parser.rs
@@ -25,6 +25,11 @@ impl<'a> Stream<'a> {
         self.data.get(self.idx)
     }
 
+    pub fn expect(&self, check: &u8) -> bool {
+        let cur = self.data.get(self.idx).unwrap();
+        cur == check
+    }
+
     pub fn slice(&self, start: usize) -> &'a [u8] {
         &self.data[start..self.idx]
     }
@@ -41,10 +46,16 @@ impl<'a> Parser<'a> {
         }
     }
 
+
     fn read_tag(&mut self) -> dom::Node {
         // we have read a b'<' already
-        let tag_name = self.read_to(b'>');
         let attributes = dom::AttrMap::new();
+        self.skip_whitespace().unwrap();
+        let tag_name = self.read_to_or(b'>', b' ');
+        self.skip_whitespace().unwrap();
+        // are we at end of tag?
+        // yes, attributes is empty
+        // no keep going until we are at end of tag
         dom::Node::elem(tag_name, attributes, Vec::new())
     }
 
@@ -58,11 +69,40 @@ impl<'a> Parser<'a> {
         }
     }
 
+    fn read_identifier(&mut self) -> &'a [u8] {
+        let start = self.stream.idx;
+        loop {
+            if let Some(char) = self.stream.current() {
+                match char {
+                    b'a'..=b'z' | b'A'..=b'Z' | b'0'..=b'9' => {
+                        self.stream.advance();
+                    },
+                    _ => {
+                        return self.stream.slice(start);
+                    }
+                }
+            }
+        }
+    }
+
     fn read_to(&mut self, needle: u8) -> &'a [u8] {
         let start = self.stream.idx;
         loop {
             if let Some(char) = self.stream.current() {
                 if *char == needle {
+                    return self.stream.slice(start)
+                } else {
+                    self.stream.advance();
+                }
+            }
+        }
+    }
+
+    fn read_to_or(&mut self, needle1: u8, needle2: u8) -> &'a [u8] {
+        let start = self.stream.idx;
+        loop {
+            if let Some(char) = self.stream.current() {
+                if *char == needle1 || *char == needle2 {
                     return self.stream.slice(start)
                 } else {
                     self.stream.advance();
@@ -93,7 +133,7 @@ mod test {
 
     #[test]
     fn test_read_tag() {
-        let test_value = "p>".as_bytes();
+        let test_value = "p >".as_bytes();
         let mut parser = Parser::new(test_value);
         let node = parser.read_tag();
         assert_eq!(node.get_tag_name(), "p".as_bytes());

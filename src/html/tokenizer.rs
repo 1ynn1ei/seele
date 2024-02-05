9d3b409efc0259b3ac8b7a8aa77689caeb817dc0
lynnlei <brooke.leichtle@gmail.com>
2024-02-05 17:53:56 +0100

    defining tokens as per spec

--- src/html/tokenizer.rs
+++ src/html/tokenizer.rs
@@ -1,5 +1,6 @@
+use std::collections::HashMap;
 //https://html.spec.whatwg.org/multipage/parsing.html#tokenization
-struct DocType<'stream> {
+pub struct DocType<'stream> {
     name: Option<&'stream [u8]>,
     public_id: Option<&'stream [u8]>,
     system_id: Option<&'stream [u8]>,
@@ -17,10 +18,27 @@ impl<'stream> DocType<'stream> {
     }
 }
 
+pub type AttrMap<'stream> = HashMap<&'stream [u8], &'stream [u8]>;
+pub struct Tag<'stream> {
+    name: &'stream [u8],
+    self_closing: bool,
+    attributes: AttrMap<'stream>
+}
+
+impl<'stream> Tag<'stream> {
+    pub fn new(name: &'stream [u8]) -> Self {
+        Self {
+            name,
+            self_closing: false, 
+            attributes: AttrMap::new(),
+        }
+    }
+}
+
 pub enum Tokens<'stream> {
-    DOCTYPE(DocType<'stream>),
-    StartTag,
-    EndTag,
+    Doctype(DocType<'stream>),
+    StartTag(Tag<'stream>),
+    EndTag(Tag<'stream>),
     Comment(&'stream [u8]),
     Character(&'stream [u8]),
     EndOfFile,

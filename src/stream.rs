ebb437cbc0ddee2d4e95c002d3a3b82a6aee3dfa
lynnlei <brooke.leichtle@gmail.com>
2024-02-09 12:01:55 +0100

    clean up unused warnings

--- src/stream.rs
+++ src/stream.rs
@@ -10,11 +10,6 @@ impl<'stream> Stream<'stream> {
             idx: 0
         }
     }
-
-    pub fn cursor(&self) -> usize {
-        self.idx
-    }
-
     pub fn advance(&mut self) {
         self.idx += 1;
     }
@@ -29,23 +24,5 @@ impl<'stream> Stream<'stream> {
 
     pub fn current(&self) -> &'stream u8 {
         &self.data[self.idx]
-    }
-
-    pub fn expect(&self, check: u8) -> bool {
-        let cur = self.data.get(self.idx).unwrap();
-        *cur == check
-    }
-
-    pub fn expect_and_advance(&mut self, check: u8) -> bool {
-        if self.expect(check) {
-            self.advance();
-            true
-        } else {
-            false
-        }
-    }
-
-    pub fn slice(&self, start: usize) -> &'stream [u8] {
-        &self.data[start..self.idx]
     }
 }

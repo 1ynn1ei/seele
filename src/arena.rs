b14de19b8cbd2d4ecde8b72669bce5e649d8ce5e
Brooke Leichtle <brooke.leichtle@gmail.com>
2024-02-17 12:16:10 +0100

    arena storage

---
+++ src/arena.rs
@@ -0,0 +1,46 @@
+pub type ArenaRef = usize;
+pub enum ArenaError {
+    IndexOutOfBounds
+}
+
+#[derive(Default)]
+pub struct Arena<T> {
+    active_pool: Vec<Option<T>>,
+    inactive_pool: Vec<ArenaRef>,
+}
+
+impl<T> Arena<T> {
+    pub fn get(&self, index: ArenaRef) -> Option<&T> {
+        match self.active_pool.get(index) {
+            Some(elem) => elem.as_ref(),
+            None => None
+        }
+    }
+
+    pub fn get_mut(&mut self, index: ArenaRef) -> Option<&mut T> {
+        match self.active_pool.get_mut(index) {
+            Some(elem) => elem.as_mut(),
+            None => None
+        }
+    }
+
+    pub fn add(&mut self, elem: T) -> ArenaRef {
+        if let Some(idx) = self.inactive_pool.pop() {
+            self.active_pool[idx] = Some(elem);
+            idx
+        } else {
+            self.active_pool.push(Some(elem));
+            self.active_pool.len() - 1
+        }
+    }
+
+    pub fn remove(&mut self, index: ArenaRef) -> Result<(), ArenaError> {
+        if index >= self.active_pool.len() {
+            Err(ArenaError::IndexOutOfBounds)
+        } else {
+            self.active_pool[index] = None;
+            self.inactive_pool.push(index);
+            Ok(())
+        }
+    }
+}
